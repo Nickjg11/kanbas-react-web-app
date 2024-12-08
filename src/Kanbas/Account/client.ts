0eb2e5371fb91a5ccd639ea2efb8eb0c33d7817e
Nick Gillespie <nickgillespie2002@gmail.com>
2024-12-08 09:24:48 -0500

    Revert "Assignment 6 first commit"
    This reverts commit 48212211d3261b3646716c99a78e7c176b12bd41.

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -2,29 +2,6 @@ import axios from "axios";
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
-export const findAllUsers = async () => {
-  const response = await axiosWithCredentials.get(USERS_API);
-  return response.data;
-};
-export const deleteUser = async (userId: string) => {
-  const response = await axios.delete( `${USERS_API}/${userId}` );
-  return response.data;
-};
-export const findUsersByRole = async (role: string) => {
-  const response = await
-    axios.get(`${USERS_API}?role=${role}`);
-  return response.data;
-};
-export const findUserById = async (id: string) => {
-  console.log("finding ", id)
-  const response = await axios.get(`${USERS_API}/${id}`);
-  console.log("found ", response.data)
-  return response.data;
-};
-export const findUsersByPartialName = async (name: string) => {
-  const response = await axios.get(`${USERS_API}?name=${name}`);
-  return response.data;
-};
 export const createCourse = async (course: any) => {
     const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
     return data;
@@ -51,10 +28,6 @@ export const profile = async () => {
   };
 export const signout = async () => {
     const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
-    return response.data;
-  };
-  export const createUser = async (user: any) => {
-    const response = await axios.post(`${USERS_API}`, user);
     return response.data;
   };
   
